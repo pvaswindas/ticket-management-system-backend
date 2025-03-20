77ecbb0992f24d4c4801e0995fdb74e3fad8b131
ASWIN DAS P V <aswinpv1801@gmail.com>
2025-03-20 10:04:22 +0530

    Add admins.py for User management
    Create administrative interface for User model with customized
    fields and display options. This improves the backend user
    management capabilities.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'role', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('email', 'role', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('Role', {'fields': ('role',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'role',
+                'is_staff',
+                'is_active'
+            ),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
