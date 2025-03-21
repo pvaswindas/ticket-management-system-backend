dd31e3b1534f8a9cc84b31e2db836a7c23d7cbf4
ASWIN DAS P V <aswinpv1801@gmail.com>
2025-03-21 16:20:48 +0530

    feat(tickets): add Ticket model
    - Created the Ticket model with title, description, priority, and status fields.
    - Added a OneToOne relationship with the User model.
    - Defined choices for priority and status with default values.

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+PRIORITY_CHOICES = [
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High')
+]
+
+STATUS_CHOICES = [
+    ('open,', 'Open'),
+    ('in-progress', 'In-Progress'),
+    ('resolved', 'Resolved')
+]
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default='low'
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='open'
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
